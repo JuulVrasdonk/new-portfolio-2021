e486a31854f81985d6ba9c1966b1430273be9677
JuulVrasdonk <56347630+JuulVrasdonk@users.noreply.github.com>
2021-10-22 22:06:59 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,6 @@
+// ************* Image reveal on hover ************* //
+
+
 if (screen.width < 800) {
     // uit met de buis
   } else {
@@ -34,10 +37,19 @@ if (screen.width < 800) {
     });
 }
 
+
+// ************* Custom cursor ************* //
+
+
 const cursor = document.querySelector('.cursor');
+const cursorIcon = document.querySelector('.cursoricon')
+const cursorBorder = document.querySelector('.cursorborder')
+
+
       document.addEventListener('mousemove', e => {
         cursor.setAttribute("style", "top: "+(e.pageY - 16)+"px; left: "+(e.pageX - 16)+"px;")
         document.querySelector(".fade").classList.remove('invisible');
+        document.querySelector(".cursor-text").classList.remove('invisible');
   })
 
 
@@ -47,5 +59,57 @@ window.onscroll = function() {
 };
 function scrolling() {
       document.querySelector(".fade").classList.add('invisible');
-      // cursor.classList.remove('transition')
+      document.querySelector(".cursor-text").classList.add('invisible');
 }
+
+
+// ************* Hover cursor text ************* //
+
+
+const aboutMeLink = document.querySelector(".about-me-link");
+
+
+aboutMeLink.addEventListener("mouseover", function (){
+  document.querySelector(".cursor-text").textContent = "- Click to get to know me!";
+  document.querySelector(".cursor-text").classList.add('text-fade-in')
+});
+
+function removeText() {
+  document.querySelector(".cursor-text").textContent = " ";
+  document.querySelector(".cursor-text").classList.remove('text-fade-in')
+}
+
+aboutMeLink.addEventListener("mouseleave", removeText);
+
+
+// ************* Work item cursor ************* //
+
+
+const workLink = document.querySelectorAll(".myworkitem");
+
+workLink.forEach((element) => {
+  element.addEventListener("mouseover", function (){
+    cursorBorder.style.opacity = "0";
+    cursorIcon.style.opacity = "0";
+  })
+})
+
+workLink.forEach((element) => {
+  element.addEventListener("mouseleave", function (){
+    cursorBorder.style.opacity = "1";
+    cursorIcon.style.opacity = "1";
+  })
+})
+
+
+// ************* Clickable cursor ************* //
+
+
+aboutMeLink.addEventListener("mouseover", function (){
+  cursorBorder.classList.add('clickable')
+});
+
+aboutMeLink.addEventListener("mouseleave", function (){
+  cursorBorder.classList.remove('clickable')
+});
+
